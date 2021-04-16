eef1d02bb61088b33f8cb0ff51f93df178e80d27
Abhishek Thomas Varghese <69757774+AbhishekRMX1901@users.noreply.github.com>
2021-04-16 17:17:12 +0530

    get ready for ssos

--- ssos_RMX1901.mk
+++ ssos_RMX1901.mk
@@ -8,35 +8,34 @@
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Inherit some common Ancient stuff
-$(call inherit-product, vendor/ancient/config/common_full_phone.mk)
+$(call inherit-product, vendor/ssos/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_OP_FILE_MANAGER := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Gapps
 TARGET_GAPPS_ARCH := arm64
 IS_PHONE := true
-ANCIENT_GAPPS := true
-
-# Official-ify
-ANCIENT_OFFICIAL := true
-FORCE_OTA := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USES_BLUR := true
-EXTRA_FOD_ANIMATIONS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := Realme
-PRODUCT_NAME := ancient_RMX1901
+PRODUCT_NAME := ssos_RMX1901
 PRODUCT_MODEL := Realme X
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210405.005 7181113 release-keys" \
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+# Fingerprint
 BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ2A.210405.005 7181113 release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC=$(BUILD_DESCRIPTION)
+
+# ShapeShift specific properties
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.ssos.cpu=SD865
